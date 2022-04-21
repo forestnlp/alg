c8af22fdf3819b2ba416e619225fb08e3913edba
forestnlp <liujie5607@163.com>
2022-04-21 12:19:05 +0800

    Changes

--- src/com/alg/sword/jz44/Solution.java
+++ src/com/alg/sword/jz44/Solution.java
@@ -7,12 +7,59 @@ public class Solution {
     /**
      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
      *
-     * 
-     * @param n int整型 
+     * @param n int整型
      * @return int整型
      */
-    public int findNthDigit (int n) {
+    public int findNthDigit(int n) {
+        if(n<10) return n;
         // write code here
-        return 0;
+
+        n-=10;
+        int digits = 2;
+
+        int m = n;
+        while (true) {
+            long cnt = count(digits);
+            if ( cnt * digits > m) break;
+            m -= cnt * digits;
+            digits++;
+        }
+
+        int start = (int) Math.pow(10,digits-1);
+        int more = m/digits;
+        int offset = m%(digits);
+
+        return String.valueOf(start+more).charAt(offset)-'0';
+    }
+
+    private long count(int level) {
+        if (level == 2) return 90;
+        return 10 * count(level - 1);
+    }
+
+    public int findNthDigit2 (int n) {
+        // write code here
+        if(n < 10)
+            return n;
+        n -= 10;
+        int digit = 2;
+        long num = 10, sum = 90;
+        while(n - digit * sum >= 0) {
+            n -= digit * sum;
+            digit++;
+            sum *= 10;
+            num *= 10;
+        }
+        int div = n / digit;
+        int mod = n % digit;
+        long now = num + div;
+        return String.valueOf(now).charAt(mod) - '0';
+    }
+
+    public static void main(String[] args) {
+        int index = (int) Math.pow(10,9);
+        System.out.println(new Solution().findNthDigit(index));
+        System.out.println(new Solution().findNthDigit2(index));
+
     }
 }
--- src/com/alg/sword/jz59/Solution2.java
+++ src/com/alg/sword/jz59/Solution2.java
@@ -1,32 +1,43 @@
 package com.alg.sword.jz59;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Solution2 {
+
+    class MyQueue{
+        private ArrayDeque<Integer> queue = new ArrayDeque<>();
+        public void push(int n ){
+            while (!queue.isEmpty()&&queue.peekLast()<n){
+                queue.pollLast();
+            }
+            queue.offerLast(n);
+        }
+
+        public int max(){
+            return queue.peekFirst();
+        }
+
+        public void pop(int n){
+            if(!queue.isEmpty()&&queue.peekFirst()==n){
+                queue.pollFirst();
+            }
+        }
+    }
     public ArrayList<Integer> maxInWindows(int [] num, int size) {
         ArrayList<Integer> ans = new ArrayList<>();
-        if(num==null||num.length==0) return ans;
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2-o1;
+        MyQueue window = new MyQueue();
+        for(int i=0;i<num.length;i++){
+            if(i<size-1){
+                window.push(num[i]);
             }
-        });
-
-        int to= size-1;
-        for(int i=0;i<=to;i++){
-            pq.offer(num[i]);
-        }
-        ans.add(pq.peek());
-
-        while (to<num.length-1){
-            to++;
-            pq.offer(num[to]);
-            pq.remove(num[to-size]);
-            ans.add(pq.peek());
+            else {
+                window.push(num[i]);
+                ans.add(window.max());
+                window.pop(num[i-size+1]);
+            }
         }
         return ans;
     }

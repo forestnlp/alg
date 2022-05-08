e1142bd87a2660eda7b7882ee42584b9f4eaa0b2
forestnlp <19085498@qq.com>
2022-05-08 11:55:24 +0800

    开始刷ladog

---
+++ src/com/alg/dog/leetcode124/Leetcode124.java
@@ -0,0 +1,15 @@
+package com.alg.dog.leetcode124;
+
+public class Leetcode124 {
+    class Solution {
+        int res = 0;
+        public int maxPathSum(TreeNode root) {
+            if(root==null) return 0;
+            int left = maxPathSum(root.left);
+            int right = maxPathSum(root.right);
+            int pathval = root.val+left+right;
+            res = Math.max(pathval,res);
+            return res;
+        }
+    }
+}

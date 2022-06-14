1a3cddd3485cde37ef1994f894fbc4eacaba410d
forestnlp <liujie5607@163.com>
2022-06-14 17:19:04 +0800

    Tree

--- src/com/alg/dog/tree/leetcode226/Solution.java
+++ src/com/alg/dog/tree/leetcode226/Solution.java
@@ -1,4 +1,21 @@
 package com.alg.dog.tree.leetcode226;
 
+import com.alg.dog.tree.TreeNode;
+
 public class Solution {
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if(root==null) return null;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+
+    }
+
 }
